d87eb1e4b4ed786d7653c883d4de1f81083b8dcd
Galin Petrov <galincho112@gmail.com>
2023-11-16 14:26:38 +0200

    initial commiit

---
+++ src/main/java/com/cloud/rebellion/demo/CloudRebellionApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.rebellion.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRebellionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRebellionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/rebellion/demo/CloudRebellionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.rebellion.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRebellionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
